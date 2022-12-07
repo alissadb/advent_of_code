adb16246eaef77319bd1b60c4ba7788fcd89c08c
Alissa de Bruijn <adebruijn@xccelerated.io>
2022-12-07 13:22:35 +0100

    Add read_split_data function to utils

--- aoc/utils.py
+++ aoc/utils.py
@@ -26,3 +26,17 @@ def read_data(file_name: str) -> list[list]:
         puzzle_input = file_obj.read()
 
     return puzzle_input
+
+def read_split_data(file_name: str) -> list[list]:
+    """Read the data from a given file name.
+
+    Args:
+        file_name (str): File name of the file to read
+
+    Returns:
+        list[list]: List of lists with the data
+    """
+    with open(file_name, mode="r", encoding="utf-8") as file_obj:
+        puzzle_input = file_obj.read().split("\n")
+
+    return puzzle_input
